a784a139bf60dce3b5c9333b49aec5fa7eee9a9a
neer123-ops <73882872+neer123-ops@users.noreply.github.com>
2022-01-09 15:42:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function navigatetv()
+{
+    window.location = "tv.html";
+}
+
+
+function navigateclock()
+{
+    window.location = "clock.html";
+}
+
+
+function navigatebooks()
+{
+    window.location = "books.html";
+}
+
+
+function navigatebottles()
+{
+    window.location = "bottles.html";
+}
+
+function navigatedustbin()
+{
+    window.location = "dustbin.html";
+}
